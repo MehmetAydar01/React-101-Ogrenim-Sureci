b0e07756020a56a0cb277fc0780f8a67ac796af9
Mehmet Aydar <ceza_mehmet8@hotmail.com>
2022-07-31 07:04:14 +0300

    update files

---
+++ src/Components/Car.js
@@ -0,0 +1,5 @@
+function Car() {
+    return <h2>Hello, I am a Car!!</h2>;
+}
+
+export default Car;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const x = 5;
+// farklı dosya oluşturup import ettik, şimdi bu dosyada import ettiğimiz componentimizi kullanabiliriz.
+import Car from './Components/Car';
 
 
 
-const myElement = <h1> {(x) ? "Hello" : "Goodbye"} </h1>
+
+/* COMPONENT */
+
+
+/* class Car extends React.Component {
+    render() {
+        return <h2> Hi, I am a Car! </h2>; // Class Component
+    }
+} */
+
+
+/*
+    function Car(props) { // Props yapısı
+        return <h2> Hi, I am a Car!! My car's color is {props.color} </h2>; // Function Component
+    }
+
+    function Garage() {
+        // <> </> yerine <div> </div> de kullanabiliriz.Eğer return içerisine başka elemanlar da yazacaksak en az bir kapsayıcı kullanmak şart. Parent eleman olacak yani.
+
+        return (
+            <> 
+                <h1>Who lives in my Garage</h1>
+                <Car color="Green"/> // Color is a Props attribute.
+            </>
+        )
+
+    }
+
+    çalışması için :
+    root.render(
+        <Garage /> 
+    );
+    kullanmamız gerek.
+*/
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    myElement
+    <Car />
 );
 
 
